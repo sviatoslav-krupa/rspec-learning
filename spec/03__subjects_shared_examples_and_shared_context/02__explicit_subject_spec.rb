c803287ba23c8f487232663b00daacc68b2b3358
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-04-15 14:17:29 +0300

    Fix typo in 'nested example' description and add tests for multiple explicit subjects, demonstrating subject overwriting behavior.

--- spec/03__subjects_shared_examples_and_shared_context/02__explicit_subject_spec.rb
+++ spec/03__subjects_shared_examples_and_shared_context/02__explicit_subject_spec.rb
@@ -22,7 +22,7 @@ describe Hash do
     expect(bob.length).to     eq(4)
   end
 
-  describe 'nexted example' do
+  describe 'nested example' do
     it 'has two key-value pairs' do
       expect(subject.length).to eq(2)
       expect(bob.length).to     eq(2)
@@ -38,3 +38,36 @@ end
 #
 #   Finished in 0.00061 seconds (files took 0.0338 seconds to load)
 #   2 examples, 0 failures
+
+# Edge cases:
+describe 'Many explicit subjects' do
+  subject { { a: 1, b: 2 } }
+  subject { { c: 3, d: 4 } }
+
+  it 'overwrites the subject' do
+    p subject #=> {c: 3, d: 4}
+  end
+end
+# OUTPUT:
+#
+#   {c: 3, d: 4}
+#   .
+# 
+#   Finished in 0.00164 seconds (files took 0.04422 seconds to load)
+#   1 example, 0 failures
+
+describe 'Explicit and named subjects' do
+  subject { { a: 1, b: 2 } }
+  subject(:bob) { { c: 3, d: 4 } }
+
+  it 'last one wins' do
+    p subject #=> {c: 3, d: 4}
+  end
+end
+# OUTPUT:
+#
+#   {c: 3, d: 4}
+#   .
+# 
+#   Finished in 0.00164 seconds (files took 0.04422 seconds to load)
+#   1 example, 0 failures
