485d382d3acb4f00a5c590301990c3b092daf0b0
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-04-16 16:51:25 +0300

    Add additional tests for change matcher to verify behavior with explicit subject references, ensuring consistency in state change assertions.

--- spec/04__built-in-matchers/07__change_matcher_spec.rb
+++ spec/04__built-in-matchers/07__change_matcher_spec.rb
@@ -10,11 +10,13 @@ describe 'change matcher' do
   it 'checks that a method changes object state' do
     # Or `expect { subject.push(4) }.to change { subject.length }.from(3).to(4)`
     expect { subject.push(4) }.to change { subject.length }.by(1)
+    expect { subject.push(4) }.to change(subject, :length).by(1)
   end
 
   it 'accepts negative arguments' do
     # Or `expect { subject.pop }.to change { subject.length }.from(4).to(3)`
     expect { subject.pop }.to change { subject.length }.by(-1)
+    expect { subject.pop }.to change(subject, :length).by(-1)
   end
 end
 # OUTPUT:
