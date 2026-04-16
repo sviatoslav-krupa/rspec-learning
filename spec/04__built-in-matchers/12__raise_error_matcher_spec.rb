64d3a913220c5cac709a85de5f621557e12b6661
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-04-16 18:13:57 +0300

    Improve formatting of warning message in raise_error matcher spec for better readability and clarity on potential false positives.

--- spec/04__built-in-matchers/12__raise_error_matcher_spec.rb
+++ spec/04__built-in-matchers/12__raise_error_matcher_spec.rb
@@ -21,7 +21,13 @@ end
 # OUTPUT:
 #
 #   raise_error matcher
-#   WARNING: Using the `raise_error` matcher without providing a specific error or message risks false positives, since `raise_error` will match when Ruby raises a `NoMethodError`, `NameError` or `ArgumentError`, potentially allowing the expectation to pass without even executing the method you are intending to call. Actual error raised was #<NameError: undefined local variable or method 'x' for #<RSpec::ExampleGroups::RaiseErrorMatcher:0x000000011f7c3bf0>>. Instead consider providing a specific error class or message. This message can be suppressed by setting: `RSpec::Expectations.configuration.on_potential_false_positives = :nothing`. Called from /Users/sviatoslav-krupa/Projects/udemy-rspec/spec/04__built-in-matchers/12__raise_error_matcher_spec.rb:12:in 'block (2 levels) in <top (required)>'.
+#   WARNING: Using the `raise_error` matcher without providing a specific error or message risks false positives,
+#   since `raise_error` will match when Ruby raises a `NoMethodError`, `NameError` or `ArgumentError`,
+#   potentially allowing the expectation to pass without even executing the method you are intending to call.
+#   Actual error raised was #<NameError: undefined local variable or method 'x'
+#   for #<RSpec::ExampleGroups::RaiseErrorMatcher:0x000000011f7c3bf0>>. Instead consider providing a specific error class or message.
+#   This message can be suppressed by setting: `RSpec::Expectations.configuration.on_potential_false_positives = :nothing`.
+#   Called from /Users/sviatoslav-krupa/Projects/udemy-rspec/spec/04__built-in-matchers/12__raise_error_matcher_spec.rb:12:in 'block (2 levels) in <top (required)>'.
 #     can check for a specific error being raised
 #     can check for a user-created error
 #
