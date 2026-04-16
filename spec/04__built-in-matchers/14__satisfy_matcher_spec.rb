d727c8773c1a7a71a883d172b57dcbc1524d09ac
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-04-16 18:52:29 +0300

    Add edge case test for satisfy matcher to validate custom error messages, highlighting failure for non-palindrome input.

--- spec/04__built-in-matchers/14__satisfy_matcher_spec.rb
+++ spec/04__built-in-matchers/14__satisfy_matcher_spec.rb
@@ -11,6 +11,18 @@ describe 'satisfy matcher' do
   it 'is a palindrome' do
     expect(subject).to satisfy { |value| value == value.reverse  }
   end
+end
+# OUTPUT:
+#
+#   satisfy matcher
+#     is a palindrome
+#
+#   Finished in 0.00107 seconds (files took 0.04736 seconds to load)
+#   1 example1, 0 failures
+
+# Edge cases:
+describe 'satisfy matcher' do
+  subject { 'racecars' }
 
   it 'can accept a custom error message' do
     expect(subject).to satisfy('be a palindrome') do |value|
@@ -21,8 +33,22 @@ end
 # OUTPUT:
 #
 #   satisfy matcher
-#     is a palindrome
-#     can accept a custom error message
+#     can accept a custom error message (FAILED - 1)
+# 
+#   Failures:
 #
-#   Finished in 0.00107 seconds (files took 0.04736 seconds to load)
-#   2 examples, 0 failures
+#     1) satisfy matcher can accept a custom error message
+#        Failure/Error:
+#          expect(subject).to satisfy('be a palindrome') do |value|
+#            value != value.reverse
+#          end
+#  
+#          expected "racecars" to be a palindrome
+#        # ./spec/04__built-in-matchers/14__satisfy_matcher_spec.rb:35:in 'block (2 levels) in <top (required)>'
+# 
+#   Finished in 0.00384 seconds (files took 0.0685 seconds to load)
+#   1 example, 1 failure
+#
+#   Failed examples:
+# 
+#   rspec ./spec/04__built-in-matchers/14__satisfy_matcher_spec.rb:34 # satisfy matcher can accept a custom error message
