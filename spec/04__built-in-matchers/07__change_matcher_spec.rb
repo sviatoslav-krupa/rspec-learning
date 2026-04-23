7209eba078668fccb1bf8afd12266d7dd7d1f152
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-04-23 12:00:29 +0300

    Refactor change matcher spec to use lambda in expectation, addressing deprecation warnings and enhancing clarity on expected syntax.

--- spec/04__built-in-matchers/07__change_matcher_spec.rb
+++ spec/04__built-in-matchers/07__change_matcher_spec.rb
@@ -15,7 +15,7 @@ describe 'change matcher' do
 
   it 'accepts negative arguments' do
     # Or `expect { subject.pop }.to change { subject.length }.from(4).to(3)`
-    expect { subject.pop }.to change { subject.length }.by(-1)
+    expect(-> { subject.pop }).to change { subject.length }.by(-1) # actually, passing lambda to expectation
     expect { subject.pop }.to change(subject, :length).by(-1)
   end
 end
@@ -24,6 +24,19 @@ end
 #   change matcher
 #     checks that a method changes object state
 #     accepts negative arguments
+#
+#   Deprecation Warnings:
+#
+#   The implicit block expectation syntax is deprecated, you should pass a block rather than an argument to `expect`
+#   to use the provided block expectation matcher or the matcher must implement `supports_value_expectations?`.
+#   e.g  `expect { value }.to change `subject.length` by -1` not `expect(value).to change `subject.length` by -1`
+#
+#   If you need more of the backtrace for any of these deprecations to
+#   identify where to make the necessary changes, you can configure
+#   `config.raise_errors_for_deprecations!`, and it will turn the
+#   deprecation warnings into errors, giving you the full backtrace.
+#
+#   1 deprecation warning total
 #
 #   Finished in 0.00127 seconds (files took 0.03325 seconds to load)
 #   2 examples, 0 failures
